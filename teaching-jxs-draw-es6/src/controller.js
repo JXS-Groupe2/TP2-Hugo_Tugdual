79ce75b83c4f78f33102794ff823b25317a44fa8
Tugdual <reboux.tugdual@gmail.com>
2018-03-30 13:50:22 +0200

    tp1 fini

--- teaching-jxs-draw-es6/src/controller.js
+++ teaching-jxs-draw-es6/src/controller.js
@@ -12,33 +12,64 @@ class Pencil {
         // Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 		document.getElementById("butRect").addEventListener("click", this.changeShape.bind(this, 0)); 
 		document.getElementById("butLine").addEventListener("click", this.changeShape.bind(this, 1));
+		document.getElementById("spinnerWidth").addEventListener("change", this.changeLineWidth.bind(this));
+		document.getElementById("colour").addEventListener("change", this.changeColour.bind(this));
 	}
 	
 	changeShape(shape) {
 		this.currentShape = shape;
 	}
 
-	changeColour(colour) {
-		
+	changeColour() {
+		this.currColour = document.getElementById("colour").value;
+	}
+
+	changeLineWidth() {
+		this.currLineWidth = document.getElementById("spinnerWidth").value;
+	}
+
+	deleteShape(id) {
+		const shape = document.getElementById("shape" + id).parentNode;
+		shape.parentNode.removeChild(shape);
+		drawing.delete(id);
+		drawing.redraw(ctx);
+	}
+
+	addShapeToList() {
+		const forme = drawing.getLastForme();
+		const list = document.getElementById("shapeList");
+		const btn = document.createElement("BUTTON");
+		const shape_id = "shape" + (drawing.getNbFormes() - 1);
+		btn.id = shape_id;
+		btn.appendChild(document.createTextNode("Delete"));
+		btn.classList.add("delete-btn");
+		const shape = document.createElement("SPAN");
+		shape.style.color = forme.getColor();
+		shape.appendChild(btn);
+		shape.appendChild(document.createTextNode(forme.getAsString()));
+		shape.appendChild(document.createElement("br"));
+		list.appendChild(shape);
+		document.getElementById(shape_id).addEventListener("click", this.deleteShape.bind(this, (drawing.getNbFormes() - 1)));
 	}
 
 	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
 	onInteractionStart(dnd) {
 		if(this.currentShape == 0 ) {
-			drawing.formes.push(new Rectangle(dnd.x_init, dnd.y_init, 0, 0, document.getElementById("spinnerWidth").value, document.getElementById("colour").value));
+			drawing.add(new Rectangle(dnd.x_init, dnd.y_init, 0, 0, this.currLineWidth, this.currColour));
 		} else {
-			drawing.formes.push(new Line(dnd.x_init, dnd.y_init, dnd.x_init, dnd.y_init, document.getElementById("spinnerWidth").value, document.getElementById("colour").value));
+			drawing.add(new Line(dnd.x_init, dnd.y_init, dnd.x_init, dnd.y_init, this.currLineWidth, this.currColour));
 		}
 		drawing.redraw(ctx, canvas);
 	}
 
 	onInteractionUpdate(dnd) {
-		drawing.formes[drawing.formes.length-1].update(dnd.x_final, dnd.y_final);
+		drawing.update(dnd.x_final, dnd.y_final);
 		drawing.redraw(ctx, canvas);
 	}
 
 	onInteractionEnd(dnd) {
-		drawing.formes[drawing.formes.length-1].update(dnd.x_final, dnd.y_final);
+		drawing.update(dnd.x_final, dnd.y_final);
+		this.addShapeToList();
 		drawing.redraw(ctx, canvas);
 	}
 }
--- teaching-jxs-draw-es6/src/model.js
+++ teaching-jxs-draw-es6/src/model.js
@@ -12,12 +12,40 @@ class Drawing {
 			elem.paint(ctx);
 		});
 	}
+
+	add(forme) {
+		this.formes.push(forme);
+	}
+
+	update(end_x, end_y) {
+		this.formes[this.formes.length-1].update(end_x, end_y);
+	}
+
+	delete(id) {
+		delete this.formes[id];
+	}
+
+	getLastForme() {
+		return this.formes[this.formes.length-1];
+	}
+
+	getNbFormes() {
+		return this.formes.length;
+	}
 }
 
 class Forme {
 	constructor(color, epaisseur) {
 		this.color = color;
 		this.epaisseur = epaisseur;
+	}
+
+	getAsString() {
+		return this.color + "; " + this.epaisseur + ";";
+	}
+
+	getColor() {
+		return this.color;
 	}
 }
 
@@ -34,6 +62,11 @@ class Rectangle extends Forme {
 		this.largeur = end_x-this.base_x;
 		this.hauteur = end_y-this.base_y;
 	}
+
+	getAsString() {
+		const forme = Object.getPrototypeOf(this.__proto__).getAsString.call(this);
+		return "Rectangle: [" + this.base_x + ", " + this.base_y + "]; [" + this.largeur + "x" + this.hauteur + "]; " + forme;
+	}
 }
 
 class Line extends Forme {
@@ -48,5 +81,10 @@ class Line extends Forme {
 	update(end_x, end_y) {
 		this.end_x = end_x;
 		this.end_y = end_y;
+	}
+
+	getAsString() {
+		const forme = Object.getPrototypeOf(this.__proto__).getAsString.call(this);
+		return "Line: [" + this.start_x + ", " + this.start_y + "] --> [" + this.end_x + ", " + this.end_y + "]; " + forme;
 	}
 }
